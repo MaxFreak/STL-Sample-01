05242d082b12ebfb5e580fd0dd3db8981c0cf2a9
MaxFreak <bl@onlinehome.de>
2013-03-04 20:10:14 +0100

    Initial Commit

---
+++ STL Sample 01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  STL Sample 01
+//
+//  Created by Bernd Lappas on 04.03.13.
+//  Copyright (c) 2013 Bernd Lappas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
