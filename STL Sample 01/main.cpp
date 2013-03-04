87f1eec9896133c7a5cb847c4f2a8570d8abedd1
MaxFreak <bl@onlinehome.de>
2013-03-04 21:47:43 +0100

    added unordered map

--- STL Sample 01/main.cpp
+++ STL Sample 01/main.cpp
@@ -8,12 +8,52 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <unordered_map>
+
+void unordered_map01();
+void vector01();
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    unordered_map01();
+    vector01();
+    
+    return 0;
+}
+
+typedef std::unordered_map<std::string,std::string> stringmap;
+
+stringmap merge (stringmap a,stringmap b)
+{
+    stringmap temp(a); temp.insert(b.begin(),b.end()); return temp;
+}
+
+void unordered_map01()
+{
+    std::cout << "\n\nunordered_map01()\n";
+    
+    // constructing unordered_maps
+    
+    stringmap first;                              // empty
+    stringmap second ( {{"apple","red"},{"lemon","yellow"}} );       // init list
+    stringmap third ( {{"orange","orange"},{"strawberry","red"}} );  // init list
+    stringmap fourth (second);                    // copy
+    stringmap fifth (merge(third,fourth));        // move
+    stringmap sixth (fifth.begin(),fifth.end());  // range
+    
+    std::cout << "sixth contains:";
+    for (auto& x: sixth) std::cout << " " << x.first << ":" << x.second;
+    std::cout << std::endl;
+}
+
+void vector01()
+{
+    std::cout << "\n\nvector01()\n";
 
     std::vector<int> v1,v2;
     
@@ -21,7 +61,7 @@ int main(int argc, const char * argv[])
     v1.push_back(11);
     v1.push_back(12);
     v1.push_back(13);
-
+    
     v2.push_back(1);
     v2.push_back(2);
     v2.push_back(3);
@@ -41,6 +81,4 @@ int main(int argc, const char * argv[])
     {
         std::cout << *ci << "\n";
     }
-    return 0;
 }
-
